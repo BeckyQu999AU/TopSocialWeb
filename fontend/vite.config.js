2ed865862615503a4b9cc9b233a5045557f8daa3
BeckyQu <beckyqu8au@gmail.com>
2025-06-18 22:30:03 +1000

    update files

---
+++ fontend/vite.config.js
@@ -0,0 +1,14 @@
+// 配置框架
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  server: {
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
